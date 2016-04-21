286b5779d0b0dbe2f5e022ddb471efd3bf6881d2
Dujeong Lee <dujeong.lee82@gmail.com>
2016-04-21 21:34:11 +0900

    Update main.cpp
    Added a function returning url for a given media format such as wemb, mp4, 3gpp, flv.

--- main.cpp
+++ main.cpp
@@ -317,3 +317,254 @@ int main(int argc, char* argv[])
     }
     return 0;
 }
+
+unsigned int get_url_with_format(const char* clip_url, char* video_request_url, unsigned int video_request_url_length, unsigned char format)
+{
+    CURL *curl;
+    CURLcode res;
+    unsigned int video_items = 0;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    if(curl) {
+	    struct ResponseBuffer* response_buffer;
+        char clip_id[32] = {0};
+        char video_info_query[512] = {0};
+        char* curl_stream_map = 0;
+        char* curl_video_item = 0;
+
+		response_buffer = (struct ResponseBuffer*)malloc(sizeof(struct ResponseBuffer));
+		if(response_buffer == 0)
+		{
+			curl_easy_cleanup(curl);
+			curl = 0;
+			curl_global_cleanup();
+			return 0;
+		}
+        {// 0. Request video information
+            sscanf(clip_url, "https://www.youtube.com/watch?v=%s", clip_id);
+            sprintf(video_info_query, "https://www.youtube.com/get_video_info?video_id=%s", clip_id);
+            curl_easy_setopt(curl, CURLOPT_URL, video_info_query);
+			memset(response_buffer->buffer, 0x0, MAX_RESPONSE_BUFFER_SIZE);
+			response_buffer->payload_size = 0;
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)response_buffer);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_payload);
+            res = curl_easy_perform(curl);
+            if(res != CURLE_OK){
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+                curl_easy_cleanup(curl);
+				curl = 0;
+                curl_global_cleanup();
+                return 0;
+            }
+        }
+
+        {// 1. Find url_encoded_fmt_stream_map from the response
+            char* url_encoded_fmt_stream_map_start = strstr(response_buffer->buffer, "url_encoded_fmt_stream_map=");
+			if(url_encoded_fmt_stream_map_start == NULL)
+			{
+                curl_easy_cleanup(curl);
+				curl = 0;
+                curl_global_cleanup();
+                return 0;
+			}
+            char* url_encoded_fmt_stream_map_end = strchr(url_encoded_fmt_stream_map_start+1, '&');
+			if(url_encoded_fmt_stream_map_end == NULL) // This is a case where url_encoded_fmt_stream_map is the last element
+			{
+				url_encoded_fmt_stream_map_end = strchr(url_encoded_fmt_stream_map_start+1, 0);
+			}
+			if(url_encoded_fmt_stream_map_end == NULL)
+			{
+                curl_easy_cleanup(curl);
+				curl = 0;
+                curl_global_cleanup();
+                return 0;
+			}
+            int curl_easy_unescape_out;
+            curl_stream_map = curl_easy_unescape(curl,
+                                                 url_encoded_fmt_stream_map_start+strlen("url_encoded_fmt_stream_map="),
+                                                 url_encoded_fmt_stream_map_end-url_encoded_fmt_stream_map_start-strlen("url_encoded_fmt_stream_map="),
+                                                 &curl_easy_unescape_out);
+			if(curl_stream_map == NULL)
+			{
+                curl_easy_cleanup(curl);
+				curl = 0;
+                curl_global_cleanup();
+                return 0;
+			}
+        }
+		free(response_buffer);
+
+        {// 2. Find video information from stream_map
+			unsigned char match = 0;
+            char* video_item_start = curl_stream_map;
+            char* video_item_end = strchr(video_item_start+1, ',');
+			if(video_item_end == NULL)
+			{
+				video_item_end = strchr(video_item_start+1, 0);
+			}
+			if(video_item_end == NULL)
+			{
+				curl_free(curl_stream_map);
+				curl_stream_map = 0;
+				curl_easy_cleanup(curl);
+				curl = 0;
+				curl_global_cleanup();
+				return 0;
+			}
+			video_item_start = video_item_start-1; // Trick...for curl_video_item = curl_easy_unescape(curl, video_item_start+1, video_item_end - video_item_start-1, &curl_easy_unescape_out);
+            while(video_item_start)
+            {
+                int curl_easy_unescape_out;
+                curl_video_item = curl_easy_unescape(curl, video_item_start+1, video_item_end - video_item_start-1, &curl_easy_unescape_out);
+				if(curl_video_item == NULL)
+				{
+					curl_free(curl_stream_map);
+					curl_stream_map = 0;
+					curl_easy_cleanup(curl);
+					curl = 0;
+					curl_global_cleanup();
+					return 0;
+				}
+				switch(format)
+				{
+				case YT_CLIP_TYPE_WEBM:
+					if(strstr(curl_video_item, "type=video/webm") != NULL)
+					{
+						goto match_found;
+					}
+					break;
+				case YT_CLIP_TYPE_MP4:
+					if(strstr(curl_video_item, "type=video/mp4") != NULL)
+					{
+						goto match_found;
+					}
+					break;
+				case YT_CLIP_TYPE_3GPP:
+					if(strstr(curl_video_item, "type=video/3gpp") != NULL)
+					{
+						goto match_found;
+					}
+					break;
+				case YT_CLIP_TYPE_FLV:
+					if(strstr(curl_video_item, "type=video/x-flv") != NULL)
+					{
+						goto match_found;
+					}
+					break;
+				}
+				video_item_start = strchr(video_item_start+1, ',');
+				if(video_item_start == NULL)
+				{
+					curl_free(curl_video_item);
+					curl_video_item = 0;
+					curl_free(curl_stream_map);
+					curl_stream_map = 0;
+					curl_easy_cleanup(curl);
+					curl = 0;
+					curl_global_cleanup();
+					return 0;
+				}
+				video_item_end = strchr(video_item_start+1, ',');
+				if(video_item_end == NULL)
+				{
+					video_item_end = strchr(video_item_start+1, 0);
+				}
+				if(video_item_end == NULL)
+				{
+					curl_free(curl_video_item);
+					curl_video_item = 0;
+					curl_free(curl_stream_map);
+					curl_stream_map = 0;
+					curl_easy_cleanup(curl);
+					curl = 0;
+					curl_global_cleanup();
+					return 0;
+				}
+				if(curl_video_item)
+				{
+					curl_free(curl_video_item);
+					curl_video_item = 0;
+				}
+            }
+			if(curl_video_item == NULL)
+			{
+				curl_free(curl_stream_map);
+				curl_stream_map = 0;
+				curl_easy_cleanup(curl);
+				curl = 0;
+				curl_global_cleanup();
+				return 0;
+			}
+match_found:
+			curl_free(curl_stream_map);
+			curl_stream_map = 0;
+        }
+
+        {// 3. Generate request url with curl_video_item_list
+			if(strlen(curl_video_item) >= video_request_url_length)
+			{
+				curl_free(curl_video_item);
+				curl_video_item = 0;
+				curl_easy_cleanup(curl);
+				curl = 0;
+				curl_global_cleanup();
+				return 0;
+			}
+			memset(video_request_url, 0x0, video_request_url_length);
+
+			char* url_start = strstr(curl_video_item, "url=");
+            if(url_start == NULL)
+            {
+				curl_free(curl_video_item);
+				curl_video_item = 0;
+				curl_easy_cleanup(curl);
+				curl = 0;
+				curl_global_cleanup();
+				return 0;
+            }
+            strcpy(video_request_url, url_start+strlen("url="));
+            video_request_url[strlen(video_request_url)] = '&';
+            strncpy(video_request_url+strlen(video_request_url), curl_video_item, url_start - curl_video_item);
+			if(video_request_url[strlen(video_request_url)-1] == '&')
+			{
+				video_request_url[strlen(video_request_url)-1] = 0;
+			}
+        }
+		curl_free(curl_video_item);
+		printf("URL = %s\n", video_request_url);
+
+        {// 5. Remove duplicate itag field. (I don't know why itag field comes twice. Anyway itag should not appear more then once.)
+			//test
+			char* second_itag_start = strstr(video_request_url, "itag=");
+			if(second_itag_start == 0)
+			{
+				curl_easy_cleanup(curl);
+				curl = 0;
+				curl_global_cleanup();
+				return 0;
+			}
+			while((second_itag_start = strstr(second_itag_start+1, "&itag=")) != NULL)
+			{
+				char* second_itag_end = strchr(second_itag_start+1, '&');
+				if(second_itag_end == NULL)
+				{
+					second_itag_end = strchr(second_itag_start+1, 0);
+				}
+				if(second_itag_end == NULL)
+				{
+					curl_easy_cleanup(curl);
+					curl = 0;
+					curl_global_cleanup();
+					return 0;
+				}
+				memmove(second_itag_start, second_itag_end, strlen(video_request_url) - (second_itag_end - second_itag_start));
+				second_itag_start = second_itag_start -1;
+			}
+        }
+		curl_easy_cleanup(curl);
+		curl = 0;
+		curl_global_cleanup();
+    }
+    return 1;
+}
